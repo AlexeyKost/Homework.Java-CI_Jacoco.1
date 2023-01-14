447c692b03636dc0a1bf9cf2d6d0ddf46a42954b
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2023-01-15 00:50:10 +0300

    Add files via upload

---
+++ src/main/java/obuchenie/MaxService.java
@@ -0,0 +1,13 @@
+package obuchenie;
+
+public class MaxService {
+
+        public int max(int a, int b) {
+            if (a > b) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+
+}
